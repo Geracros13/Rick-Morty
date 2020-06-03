48f9739d152d21376cc3392a89fda4d90c5b7898
Gerardo Quiñonez <gerardo01302011@gmail.com>
2020-06-03 00:43:50 -0600

    Add settings to pagination

--- src/components/useGetData.js
+++ src/components/useGetData.js
@@ -1,15 +1,30 @@
 import { useState, useEffect } from "react";
 
-const api = "https://rickandmortyapi.com/api/character/?page=3";
+// const api = "https://rickandmortyapi.com/api/character/?page=3";
+
+// 	const [data, setData] = useState([]);
+
+// 	useEffect(() => {
+// 		fetch(api)
+// 			.then((response) => response.json())
+// 			.then((data) => setData(data));
+// 	}, []);
 
 const useGetData = () => {
+	const api = "https://rickandmortyapi.com/api/character/?page=3";
+
 	const [data, setData] = useState([]);
 
 	useEffect(() => {
-		fetch(api)
-			.then((response) => response.json())
-			.then((data) => setData(data));
+		obtenerDatos();
 	}, []);
+
+	const obtenerDatos = async () => {
+		const datos = await fetch(api);
+		const personajes = await datos.json();
+
+		setData(personajes);
+	};
 
 	return data;
 };
