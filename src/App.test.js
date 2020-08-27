13dbd2d11514db27e691c69e5ace7eb1ece3afc0
Gerardo Quiñonez <gerardo01302011@gmail.com>
2020-08-27 12:59:59 -0600

    Agregando ultimos cambios

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders text", () => {
-	const { getByText } = render(<App />);
-	const prueba = getByText(/Probando/i);
-	expect(prueba).toBeInTheDocument();
-});
