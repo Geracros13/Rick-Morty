ccc27dcd30d6a38fa3615f0855b629495f526c23
Gerardo Quiñonez <gerardo01302011@gmail.com>
2020-08-27 12:58:53 -0600

    Agregando ultimos cambios

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 
-const ContainerBar = styled.div`
+const Container = styled.div`
 	width: 100%;
-	height: 75px;
+	height: 150px;
 	background-color: #212121;
 	display: flex;
 	justify-content: center;
@@ -13,10 +13,10 @@ const ContainerBar = styled.div`
 const H2 = styled.h2`
 	color: #f4f6ff;
 	font-size: 3em;
-	margin: 0 5px 0 5px;
+	margin: 0;
 	letter-spacing: 0.8px;
 	font-weight: 500;
-	padding: 30px;
+	padding-top: 30px;
 	text-align: center;
 	@media only screen and (min-width: 320px) and (max-width: 720px) {
 		font-size: 2em;
@@ -25,9 +25,9 @@ const H2 = styled.h2`
 
 const Bar = () => {
 	return (
-		<ContainerBar>
+		<Container>
 			<H2>The Rick & Morty</H2>
-		</ContainerBar>
+		</Container>
 	);
 };
 
